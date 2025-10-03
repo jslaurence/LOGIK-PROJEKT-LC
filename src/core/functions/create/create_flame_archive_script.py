5504e57c26462a5ac854db7f3f2838ca3aaa9ba0
jslaurence <jslaurence@laurencecr.com>
2025-10-03 23:05:04 +0100

    refactor: Update flame archive script path
    Modifies create_flame_archive_script.py to save scripts to {logik_projekt_path}/support/flame/archive/scripts/.

--- src/core/functions/create/create_flame_archive_script.py
+++ src/core/functions/create/create_flame_archive_script.py
@@ -44,6 +44,7 @@ def create_flame_archive_script(projekt_summary_data: dict):
 
     tgt_flame_archive_dir = os.path.join(
         the_projekt_dir,
+        'support',
         'flame',
         'archive',
     )
